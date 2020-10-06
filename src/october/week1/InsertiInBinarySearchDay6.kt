4a0b2784f786622cdffbee4f5497feb01fb88608
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-06 09:29:44 +0200

    add problem Insert In Binary Search  BST day6

---
+++ src/october/week1/InsertiInBinarySearchDay6.kt
@@ -0,0 +1,19 @@
+package october.week1
+
+import TreeNode
+
+/**
+
+You are given the root node of a binary search tree (BST)
+and a value to insert into the tree. Return the root node of the BST after the insertion.
+It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion,
+as long as the tree remains a BST after insertion. You can return any of them.
+ **/
+
+fun insertIntoBST(root: TreeNode?, `val`: Int): TreeNode? {
+    return root
+
+
+}
