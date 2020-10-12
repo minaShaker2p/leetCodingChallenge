4d8d3ea8f63f3fac44c5d0fa2c64d885d64bf11c
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-12 10:25:46 +0200

    add problem Buddy Strings

---
+++ src/main/october/week2/BuddyStrings.kt
@@ -0,0 +1,34 @@
+package main.october.week2
+
+/**
+Given two strings A and B of lowercase letters, return true if you can swap two letters in A
+so the result is equal to B, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed)
+such that i != j and swapping the characters at A[i] and A[j].
+For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+
+Example 1:
+
+Input: A = "ab", B = "ba"
+Output: true
+Explanation: You can swap A[0] = 'a' and A[1] = 'b' to get "ba", which is equal to B.
+Example 2:
+
+Input: A = "ab", B = "ab"
+Output: false
+Explanation: The only letters you can swap are A[0] = 'a' and A[1] = 'b', which results in "ba" != B.
+Example 3:
+
+Input: A = "aa", B = "aa"
+Output: true
+Explanation: You can swap A[0] = 'a' and A[1] = 'a' to get "aa", which is equal to B.
+Example 4:
+
+Input: A = "aaaaaaabc", B = "aaaaaaacb"
+Output: true
+ **/
+
+fun buddyStrings(A: String, B: String): Boolean {
+    return false
+}
