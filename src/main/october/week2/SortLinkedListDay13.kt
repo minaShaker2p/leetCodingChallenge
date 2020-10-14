03304d0275af4d47e5422657677680447b29f2ca
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-14 10:25:37 +0200

    solve problem sort linked list

--- src/main/october/week2/SortLinkedListDay13.kt
+++ src/main/october/week2/SortLinkedListDay13.kt
@@ -21,5 +21,36 @@ Input: head = []
 Output: []
  */
 fun sortList(head: ListNode?): ListNode? {
-    return head
+    if (head?.next == null)
+        return head
+    var preSlow: ListNode? = null
+    var slow: ListNode? = head
+    var fast: ListNode? = head
+
+    while (fast?.next != null) {
+        preSlow = slow
+        slow = slow?.next
+        fast = fast.next?.next
+    }
+    preSlow?.next = null
+    return mergeTwoList(sortList(head), sortList(slow))
+}
+
+fun mergeTwoList(list1: ListNode?, list2: ListNode?): ListNode? {
+    var l1 = list1
+    var l2 = list2
+    val temp: ListNode? = ListNode(-1)
+    var current = temp
+    while (l1 != null && l2 != null) {
+        if (l1.`val` < l2.`val`) {
+            current?.next = l1
+            l1 = l1.next
+        } else {
+            current?.next = l2
+            l2 = l2.next
+        }
+        current = current?.next
+    }
+    current?.next = l1 ?: l2
+    return temp?.next
 }
