4da2f7b1e55209a06e13ed676ea679aa354cfd4d
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-16 09:42:02 +0200

    add problem RotateArray Day15 with solution

---
+++ src/main/october/week3/RotateArrayDay15.kt
@@ -0,0 +1,40 @@
+package main.october.week3
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ */
+fun rotate(numbers: IntArray, k: Int): Unit {
+
+    var i =0;
+    val size=numbers.size
+    while(i<k)
+    {
+        val temp=numbers[size-1]
+        for(j in size-1 downTo 1) {
+        numbers[j]=numbers[j-1]
+        }
+        numbers[0]=temp
+        i++
+    }
+}
