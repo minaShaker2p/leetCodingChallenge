8b4bd043b547d896fbbdc4ad3c4ae467ae271a7e
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-16 09:28:13 +0200

    solve problem HouseRobber || day 14

--- src/main/october/week2/HouseRobber||Day14.kt
+++ src/main/october/week2/HouseRobber||Day14.kt
@@ -1,5 +1,7 @@
 package main.october.week2
 
+import kotlin.math.max
+
 /*
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
@@ -25,5 +27,28 @@ Output: 0
 
 
 fun rob(nums: IntArray): Int {
-    return 0
+
+    if (nums.isEmpty())
+        return 0
+    if (nums.size == 1)
+        return nums[0]
+
+    return max(robHouse1(nums.copyOfRange(0,nums.size-1)),robHouse1(nums.copyOfRange(1,nums.size)))
+}
+
+private fun robHouse1(nums: IntArray): Int {
+    if (nums.isEmpty())
+        return 0
+    if (nums.size == 1)
+        return nums[0]
+    if (nums.size == 2)
+        return max(nums[0], nums[1])
+
+    var d = IntArray(nums.size)
+    d[0] = nums[0]
+    d[1] = max(nums[0], nums[1])
+    for (i in 2 until nums.size) {
+        d[i] = max(nums[i] + d[i - 2], d[i-1])
+    }
+    return d[nums.size - 1]
 }
